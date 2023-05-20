228d8fccc2180c2f1d7fab5aa6e34eaa77208b4c
Jordan <j.r.noble@live.co.uk>
2023-05-20 09:59:42 +0100

    linting initialised

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 const app = require('./src/app');
 
 const APP_PORT = process.env.PORT || 3000;
 
 app.listen(APP_PORT, () => {
-  console.log(`App is listeing on port ${APP_PORT}`);
-})
+  console.log(`App is listening on port ${APP_PORT}`);
+});
--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (__, res) => {
   res.send('Hello World!');
-})
+});
 
 module.exports = app;
